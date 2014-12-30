e4a484260186be1c0410c8972bd9066df98a2fe7
Sheleng <1323736312@qq.com>
2014-12-30 15:56:23 +0800

    添加MAC算法支持

--- src/main/java/com/sheleng/java/commontools/codec/digest/MessageDigestAlgorithms.java
+++ src/main/java/com/sheleng/java/commontools/codec/digest/MessageDigestAlgorithms.java
@@ -6,13 +6,14 @@ package com.sheleng.java.commontools.codec.digest;
  * @author Sheleng
  * @version 0.0.1
  */
-public class MessageDigestAlgorithms {
+class MessageDigestAlgorithms {
 
     private MessageDigestAlgorithms() {
         throw new RuntimeException("MessageDigestAlgorithms.class can't be instantiated");
     }
 
     public static final String MD2 = "MD2";
+    public static final String MD4 = "MD4";
     public static final String MD5 = "MD5";
 
     public static final String SHA_1 = "SHA-1";
@@ -20,4 +21,13 @@ public class MessageDigestAlgorithms {
     public static final String SHA_256 = "SHA-256";
     public static final String SHA_384 = "SHA-384";
     public static final String SHA_512 = "SHA-512";
+
+    public static final String HMAC_MD2 = "HmacMD2";
+    public static final String HMAC_MD4 = "HmacMD4";
+    public static final String HMAC_MD5 = "HmacMD5";
+    public static final String HMAC_SHA1 = "HmacSHA1";
+    public static final String HMAC_SHA224 = "HmacSHA224";
+    public static final String HMAC_SHA256 = "HmacSHA256";
+    public static final String HMAC_SHA384 = "HmacSHA384";
+    public static final String HMAC_SHA512 = "HmacSHA512";
 }
